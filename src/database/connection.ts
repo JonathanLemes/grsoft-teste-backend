436ff655216257a384d99e38eaa9fb65b07c38b2
JonathanLemes <JonathanLemes@unifei.edu.br>
2020-11-25 23:33:03 -0300

    fix(connection): connection parameters modified
    now the connection parameters are all included in ormconfig.json

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,12 +1,5 @@
 import { createConnection } from 'typeorm';
 
-createConnection({
-    type: "mysql",
-    host: process.env.CLEARDB_HOST || "",
-    port: 3306,
-    username: process.env.CLEARDB_USERNAME || "",
-    password: process.env.CLEARDB_PASSWORD || "",
-    database: process.env.CLEARDB_DATABASE || "",
-}).catch((err) => {
+createConnection().catch((err) => {
     console.log(err);
 });
